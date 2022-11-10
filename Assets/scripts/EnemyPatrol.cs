c932983f5bf2934478ab9ce128f34cf4bc9f1c40
TCook0 <cooktravis2000@gmail.com>
2022-11-09 22:37:58 -0600

    Added ScanbotAI & Patrol

---
+++ Assets/scripts/EnemyPatrol.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [Header ("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    [Header("Idle Behavior")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+
+    private void Awake()
+    {
+       initScale = enemy.localScale;
+    }
+
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if(enemy.position.x >= leftEdge.position.x)
+            {
+                MoveInDirection(-1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if(enemy.position.x <= rightEdge.position.x)
+            {
+                MoveInDirection(1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+    }
+
+    private void DirectionChange()
+    {
+        idleTimer += Time.deltaTime;
+
+        if(idleTimer > idleDuration)
+            movingLeft = !movingLeft;
+    }
+
+    private void MoveInDirection(int _direction)
+    {
+        idleTimer = 0;
+
+        //Make enemy face direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction * -1, 
+            initScale.y, initScale.z);
+
+        //Move in direction
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
+            enemy.position.y, enemy.position.z);
+    }
+}
