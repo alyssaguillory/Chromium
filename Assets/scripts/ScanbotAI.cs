61624aa5e5e4fb139e54b62ee3cae3bac1a4553a
Kambo2320 <kburke@cougarnet.uh.edu>
2022-11-09 22:46:41 -0600

    Added Travis' scan bot ai to my code

---
+++ Assets/scripts/ScanbotAI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanbotAI : MonoBehaviour
+{
+    [Header("Attack Parameter")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private Animator anim;
+
+
+
+    private void Awake()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight
+        if (PlayerInSight())
+        {
+            if (cooldownTimer > attackCooldown)
+            {
+                cooldownTimer = 0;
+                
+                //anim.SetTrigger(" **Name of attack animation goes here** ");
+            }
+        }
+
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * range, boxCollider.bounds.size.z), 
+            0, Vector2.left, 0, playerLayer);
+
+        if(hit.collider != null)
+        {
+            //player's health takes damage 
+        }
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * range, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            //Damage player health
+        }
+    }
+}
