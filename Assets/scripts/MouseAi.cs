b13ea61d6315892e3120b754320c52c9b5da06b6
MadDove <zahoover@gmail.com>
2022-11-15 04:04:26 -0600

    Greatly improved mouse AI
    Almost all parts are done, just need to be able to hit the player, and to be stunned.

--- Assets/scripts/MouseAi.cs
+++ Assets/scripts/MouseAi.cs
@@ -21,6 +21,7 @@ public class MouseAi : MonoBehaviour
     public SpriteRenderer[] ears;
 
     bool isStunned = false;
+    bool isElectric = false;
     bool active = false;
     public float stunDur = 0.0f;
     bool isGrounded = false;
@@ -31,9 +32,12 @@ public class MouseAi : MonoBehaviour
     public float outerEdge = 2.5f;
     public float activateArea = 5.0f;
     public float checkRadius = 1.0f;
+    public float Recharge = 2.5f;
     public LayerMask whatIsGround;
     public int State = 0;
-
+    public Vector2 targetDistance;
+    bool dealtDamage;
+    float padTime;
 
     void Start()
     {
@@ -51,20 +55,15 @@ public class MouseAi : MonoBehaviour
     }
     private void Update()
     {
-        //When the player gets into the active range of the mouse, it will activate.
-        Vector2 targetDistance = (Vector2)target.position - (Vector2)rb.position;
-        if (!active && targetDistance.magnitude < activateArea && !Dead)
-        {
-            active = true;
-            InvokeRepeating("UpdatePath", 0f, 0.5f);
-            //Activate the sprite renderer
-            foreach (SpriteRenderer a in ears)
-            {
-                a.enabled = true;
-            }
-            //Do particle effects
-        }
+        //Check the distance to the target and make sure the mouse is grounded
+        targetDistance = (Vector2)target.position - (Vector2)rb.position;
         isGrounded = Physics2D.OverlapCircle(rb.position, checkRadius, whatIsGround);
+        //Call States
+        if (State < 2)
+        {
+            States();
+        }
+        
         //Have the mouse look in the direction of the player
         if(targetDistance.x > 0 && !Dead) { transform.eulerAngles = new Vector2(0, 180); } else if (targetDistance.x < 0) { transform.eulerAngles = new Vector2(0, 0); }
     }
@@ -83,15 +82,12 @@ public class MouseAi : MonoBehaviour
         {
             reachedEndOfPath = false;
         }
-        Vector2 targetDistance = (Vector2)target.position - (Vector2)rb.position;
-        //if the mouse is near the jump zone, have it jump
-        if (targetDistance.magnitude < safeSpace && isGrounded && active && !Dead)
+        if (State >= 2)
         {
-            //else have it continue to path
-            Vector2 force = Vector2.up * jumpForce;
-            rb.AddForce(force);
-
-        } else if (active && isGrounded) { FollowPath(); }
+            targetDistance = (Vector2)target.position - (Vector2)rb.position;
+            States();
+        }
+        
     }
 
     void UpdatePath()
@@ -122,23 +118,114 @@ public class MouseAi : MonoBehaviour
         switch (State)
         {
             case 0: //Hide
+                Hidden();
                 //while in state zero, we should be checking that Player is not within shpere of influence, if it is change state to 2 and play electric
                 break;
             case 1: //Die
                 Die();
                 break;
             case 2: //Pursue
-                //while in state one, the mouse should rush towards the player and jump, when they get within range, they go to state
+                Pursue();
+                //while in state one, the mouse should rush towards the player and jump, when they get within range, they go to state 4
                 break;
             case 3: //Stunned
+                Stunned();
                 break;
             case 4: //Attack
+                Attack();
                 //While in this state if has enough time left for electricity, then jump at the player. Else state = 5
                 break;
             case 5: //Flee
+                Flee();
                 //
                 break;
         }
+    }
+
+    void Hidden()
+    {
+        if (!active && targetDistance.magnitude < activateArea && !Dead)
+        {
+            active = true;
+            InvokeRepeating("UpdatePath", 0f, 0.5f);
+            //Activate the sprite renderer
+            foreach (SpriteRenderer a in ears)
+            {
+                a.enabled = true;
+            }
+            State = 2;
+            isElectric = true;
+            //Do particle effects
+        }
+    }
+
+    void Pursue()
+    {
+        if (path == null)
+            return;
+        FollowPath();
+        
+        //if the mouse is near the jump zone, have it jump
+        if (targetDistance.magnitude < safeSpace && isGrounded && active && !Dead)
+        {
+            //else have it continue to path
+            Vector2 force = Vector2.up * jumpForce;
+            rb.AddForce(force);
+            State = 4;
+            padTime = 0.5f;
+            Attack();
+        }
+        else if (!isElectric) { Flee(); }
+        else if (active && isGrounded) { FollowPath(); }
+        
+    }
+
+    void Stunned()
+    {
+
+    }
+
+    void Attack()
+    {
+        if (isGrounded && padTime<0.5)
+        {
+            State = 5;
+            Recharge = 2.5f;
+            return;
+        }
+        if (rb.gameObject.GetComponent<Collider2D>().bounds.Intersects(target.gameObject.GetComponent<Collider2D>().bounds))
+        {
+            Debug.Log("Bounds intersecting");
+            dealtDamage = true;
+        }
+        padTime -= Time.deltaTime;
+    }
+
+     void Flee()
+    {
+        if(Recharge > 0) { 
+            Recharge -= Time.deltaTime;
+            if(targetDistance.x > 0)
+            {
+                if (Mathf.Abs(targetDistance.x) > activateArea)
+                {
+                    rb.AddForce(Vector2.right * speed * 0.75f * Time.deltaTime);
+                } else
+                {
+                    rb.AddForce(Vector2.left * speed * 0.75f * Time.deltaTime);
+                }
+            } else if(targetDistance.x < 0)
+            {
+                if (Mathf.Abs(targetDistance.x) < activateArea)
+                {
+                    rb.AddForce(Vector2.right * speed * 0.75f * Time.deltaTime);
+                }
+                else
+                {
+                    rb.AddForce(Vector2.left * speed * 0.75f * Time.deltaTime);
+                }
+            } 
+        } else { State = 2; dealtDamage = false; }
     }
 
     void OnPathComplete(Path p)
@@ -157,6 +244,7 @@ public class MouseAi : MonoBehaviour
         {
             Gizmos.DrawSphere(rb.position, activateArea);
             Gizmos.DrawSphere(target.position, safeSpace);
+            Gizmos.DrawSphere(rb.position, checkRadius);
         }
     }
 
