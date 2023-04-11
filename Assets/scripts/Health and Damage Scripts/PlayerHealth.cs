492393aabd82b63b170e56f7b22045e85de360b0
ZTHENRIC <70232641+ZTHENRIC@users.noreply.github.com>
2023-04-11 17:05:54 -0500

    Added player hurt/die sounds
    -Added player hurt and die sfx
    -Added a WIP wait after player dies so it doesn't instantly restart scene. This allows for sounds to be played fully (and an animation to play if added).

--- Assets/scripts/Health and Damage Scripts/PlayerHealth.cs
+++ Assets/scripts/Health and Damage Scripts/PlayerHealth.cs
@@ -11,14 +11,23 @@ public class PlayerHealth : Health
     public float regenWait;
     IEnumerator regen;
     public GameMaster gm;
+    AudioSource[] audioData;
+    AudioSource audioHurt;
+    AudioSource audioDie;
+
     private void Start()
     {
         CurrBars = HealthBars;
         while (healthSpawner.Gears.Count < HealthBars) { healthSpawner.AddBar(); };
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+
+        audioData = GetComponents<AudioSource>();
+        audioHurt = audioData[0];
+        audioDie = audioData[1];
     }
     public override void Die()
     {
+        audioDie.Play();
         if (CurrBars > 1)
         {
             CurrBars--;
@@ -26,13 +35,16 @@ public class PlayerHealth : Health
             healthSpawner.redoHealth();
         }
         else {
+            StartCoroutine(waiter());
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             //transform.position = gm.lastCheckPointPos;
         }
+        
         Debug.Log("You Died");
     }
     public override void Damage(float damage)
     {
+        audioHurt.Play();
         if (regen != null) { StopCoroutine(regen); }
         regen = regenarate(regenWait);
         StartCoroutine(regen);
@@ -55,7 +67,14 @@ public class PlayerHealth : Health
     void OnParticleCollision(GameObject other)
     {
         //Debug.Log("Hit");
+        audioHurt.Play();
         StartCoroutine(DamageWithInvincible(0.15f, 3.0f));
     }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(4);
+    }
+
 }
 
